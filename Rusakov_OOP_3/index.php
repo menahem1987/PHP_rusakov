ce2fdc3598cae535797c0ca016f55f0228034ebf
menahem1987 <51406197+menahem1987@users.noreply.github.com>
2020-05-21 12:00:53 +0300

    Add files via upload

---
+++ Rusakov_OOP_3/index.php
@@ -0,0 +1,10 @@
+<?php
+    class Point {
+        public $x;
+        public $y;
+    }
+    $point1 = new Point();
+    $point1 -> x = 5;
+    $point1 -> y = 3;
+    echo $point1->x, $point1->y;
+?>
