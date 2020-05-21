a6b8a6d1bb668d7a0535f41afd2231dcde843642
menahem1987 <51406197+menahem1987@users.noreply.github.com>
2020-05-21 13:26:53 +0300

    Create index4.php

---
+++ Rusakov_OOP_3/index4.php
@@ -0,0 +1,31 @@
+<?php
+    class Point {
+        public $x;
+        public $y;
+        public function __construct($x, $y)
+        {
+            $this->x = $x;
+            $this->y = $y;
+        }
+        public function getx(){
+            return $this->x;
+        }
+        public function gety(){
+            return $this->y;
+        }
+        public function setx($x){$this->x = $x;}
+        public function sety($y){$this->y = $y;}
+    }
+    $point = new Point('5','3');
+    echo $point->x;
+    echo $point->y.'<br/>';
+
+    echo $point->getx();
+    echo $point->gety();
+
+    echo $point->setx(4);
+    echo $point->sety(67);
+    echo '<br/>';
+    echo $point->getx();
+    echo $point->gety();
+?>
