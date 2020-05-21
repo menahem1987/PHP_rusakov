58bef116f283f8c052ab8aa8a3ca57935be46ffc
menahem1987 <51406197+menahem1987@users.noreply.github.com>
2020-05-21 14:12:06 +0300

    Update index5.php

--- Rusakov_OOP_3/index5.php
+++ Rusakov_OOP_3/index5.php
@@ -2,11 +2,6 @@
     class Point {
         private $x;
         private $y;
-        public function __construct($x, $y)
-        {
-            $this->x = $x;
-            $this->y = $y;
-        }
         public function getx(){
             return $this->x;
         }
@@ -16,13 +11,11 @@
         public function setx($x){$this->x = $x;}
         public function sety($y){$this->y = $y;}
     }
-    $point = new Point('5','3');
-    echo $point->getx();
-    echo $point->gety();
+    $point = new Point();
 
     echo $point->setx(4);
     echo $point->sety(67);
-    echo '<br/>';
+
     echo $point->getx();
     echo $point->gety();
 ?>
