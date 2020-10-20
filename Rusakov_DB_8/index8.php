4d7af31839611807e5cf2ce750278bdbfde510ee
menahem1987 <51406197+menahem1987@users.noreply.github.com>
2020-10-20 14:47:01 +0300

    Update index8.php

--- Rusakov_DB_8/index8.php
+++ Rusakov_DB_8/index8.php
@@ -8,15 +8,13 @@
     if ($mysqli->connect_errno) exit('Ошибка соединения БД');
     $mysqli->set_charset('utf8');
     $mysqli->query('CREATE DATABASE `chaspoj`');
-    $mysqli->close();
-    $mysqli = @new mysqli(DB_Host, DB_User, DB_Password, 'chaspoj');
     if ($mysqli->connect_errno) exit('Ошибка соединения БД');
-    $mysqli->query("CREATE TABLE `time zone` (
+    $mysqli->query("CREATE TABLE `chaspoj`.`time zone` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
   `offset` int(10) UNSIGNED NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8");
-    $mysqli->query("INSERT INTO `time zone` (`id`, `title`, `offset`) VALUES
+    $mysqli->query("INSERT INTO `chaspoj`.`time zone` (`id`, `title`, `offset`) VALUES
 (1, 'Москва/Moscow', 10800),
 (2, 'Саратов/Saratov', 14400),
 (3, 'Екатеринбург/Ekaterinburg', 18000)");
